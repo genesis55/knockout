e2ed1fd548d052d1c50d66d06f85fbed64f3bb9e
genesis55 <cpkneisler@gmail.com>
2014-09-02 15:16:49 -0400

    Add Patient and Treatment Arm

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,8 +20,36 @@ var patient = [{
     "diseases" : [],
     "registrationDate" : "2014-08-27T11:06:12.305-04:00"
 }];
+var treatmentArm = [{
+    "_id" : "3",
+    "_class" : "gov.match.model.TreatmentArm",
+    "name" : "NOMID",
+    "description" : "Neonatal-onset multisystem inflammatory disease, is a rare disease that is often fatal.",
+    "drugs" : [ 
+        {
+            "_id" : "1",
+            "name" : "Afatinib",
+            "description" : "Afatinib",
+            "drugClass" : "angiokinase inhibitor"
+        }, 
+        {
+            "_id" : "2",
+            "name" : "tylenol",
+            "description" : "tylenol",
+            "drugClass" : "angiokinase inhibitor"
+        }
+    ],
+    "treatmentArmStatus" : "PENDING",
+    "statusLog" : {
+        "1408385797042" : "PENDING"
+    },
+    "dateCreated" : "2014-08-18T14:16:37.042-04:00"
+}];
 
-function AppViewModel() {
+function PatientViewModel() {
+    this._id = patient[0]._id;
+	this.patientSequenceNumber = ko.observable(patient[0].patientSequenceNumber);
+	this.registrationDate = patient[0].registrationDate;
     this.firstName = ko.observable("Bert");
     this.lastName = ko.observable("Bertington");
     this.fullName = ko.computed(function() {
@@ -29,8 +57,33 @@ function AppViewModel() {
 	}, this);
 }
 
+function TreatmentArmViewModel() {
+    this._id = treatmentArm[0]._id;
+	this.name = ko.observable(treatmentArm[0].name);
+	this.description = ko.observable(treatmentArm[0].description);
+	this.treatmentArmStatus = ko.observable(treatmentArm[0].treatmentArmStatus);
+	this.availableStatus = ['PENDING', 'APPROVED', 'EXEMPT', 'DENIED'];
+	this.chosenStatus = ko.observableArray([treatmentArm[0].treatmentArmStatus]);
+	this.countries = ['Japan', 'Bolivia', 'New Zealand'];
+    this.selectedCountry = ko.observable('Latvia');
+}
+
 
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
-console.log("Hello");
+var koPatient = ko.applyBindings(new PatientViewModel(), document.getElementById('match-patient'));
+var koTreatmentArm = ko.applyBindings(new TreatmentArmViewModel(), document.getElementById('match-treatmentArm'));
+console.log("Hello Knockout");
 console.dir(patient);
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#btn-patient').on('click', function() {
+    	console.log('clicked on btn-patient');
+    	$('#match-patient').show();
+    	$('#match-treatmentArm').hide();
+    });
+    $('#btn-treatmentArm').on('click', function() {
+    	console.log('clicked on btn-treatmentArm');
+    	$('#match-treatmentArm').show();
+    	$('#match-patient').hide();
+    });
+});
